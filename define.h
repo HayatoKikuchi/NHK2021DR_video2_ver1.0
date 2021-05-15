f9ee408c9e9f23f6cf2c94cc00bde611147bd28f
菊池隼 <e51190405b@edu.teu.ac.jp>
2021-05-15 17:37:24 +0900

    円周率の定義の変更

--- define.h
+++ define.h
@@ -1,7 +1,7 @@
 #ifndef DEFINE_h
 #define DEFINE_h
 
-#include "Arduino.h"
+#include <Arduino.h>
 
 struct coords{
     double x;
@@ -63,10 +63,9 @@ struct coords_4{
 
 // 制御周期
 #define INT_TIME			( 0.01 )//( 0.001 )
-#define PI_ ( 3.1415926 )
 
 // 自己位置推定用エンコーダ関連
-#define _2PI_RES4   ( 2 * PI_ / (4*100) ) // 分解能 100ppr
+#define _2PI_RES4   ( 2 * PI / (4*100) ) // 分解能 100ppr
 #define RADIUS_X    ( 0.0375/2.0 )    
 #define RADIUS_Y    ( 0.0375/2.0 )
 
@@ -85,12 +84,12 @@ struct coords_4{
     #define MECANUM_HANKEI		( 0.127 / 2.0 ) // 駆動輪の半径
     #define MECHANUM_A	( 0.573/2.0 ) // 外側のホイール a[m]
     #define MECHANUM_B	( 0.540/2.0 ) // 外側のホイール b[m]
-    #define _4RES_2PI_WHEEL ( 4 * RES_MECHANUM / ( 2.0*PI_ ) ) // 駆動輪の角速度[rad/s]からRoboClawの指令値[pulses/s]に変換するための定数(1024ppr)
-    #define _4RES_2PI_WALL ( 4 * RES_WALL / ( 2.0*PI_ ) ) // 壁越え機構の角速度[rad/s]からRoboClawの指令値[pulses/s]に変換するための定数(1024ppr)
+    #define _4RES_2PI_WHEEL ( 4 * RES_MECHANUM / ( 2.0*PI ) ) // 駆動輪の角速度[rad/s]からRoboClawの指令値[pulses/s]に変換するための定数(1024ppr)
+    #define _4RES_2PI_WALL ( 4 * RES_WALL / ( 2.0*PI ) ) // 壁越え機構の角速度[rad/s]からRoboClawの指令値[pulses/s]に変換するための定数(1024ppr)
     #define GEARRATIO_WHEEL ( 1.0 ) // 駆動輪のギヤ比
     #define GEARRATIO_WALL ( 3.0 )// 壁越え機構ギヤ比
     #define MAXVEL ( 1.0 )
-    #define MAXOMEGA ( PI_ / 3.0 * 2.0 ) // 2/3π [rad/s]
+    #define MAXOMEGA ( PI / 3.0 * 2.0 ) // 2/3π [rad/s]
     #define KAKUKASOKUDO_WALL ( 471.2389 ) //壁越えモータの角加速度rad/ss
     #define DISTANCE_WALL ( 0.180 )  //壁越えに必要な距離[m](機構が90度回転する)
 #elif DRIVE_MODE == DRIVE_DUALWHEEL
@@ -113,12 +112,12 @@ struct coords_4{
     #define MECHANUM_HANKEI_B_INSIDE	( 0.26023 ) // 内側のホイール b[m]
     #define MECHANUM_HANKEI_A	( MECHANUM_HANKEI_A_OUTSIDE ) // 4輪接地のa[m] (内側と外側どちらとも等しい)
     #define MECHANUM_HANKEI_B	(( MECHANUM_HANKEI_B_OUTSIDE + MECHANUM_HANKEI_B_INSIDE ) / 2.0 ) // 4輪接地のb[m]
-    #define _4RES_2PI_WHEEL ( 4 * 3 / ( 2.0*PI_ ) ) // 駆動輪の角速度[rad/s]からRoboClawの指令値[pulses/s]に変換するための定数(3ppr)
-    #define _4RES_2PI_WALL ( 4 * 3 / ( 2.0*PI_ ) ) // 壁越え機構の角速度[rad/s]からRoboClawの指令値[pulses/s]に変換するための定数(3ppr)
+    #define _4RES_2PI_WHEEL ( 4 * 3 / ( 2.0*PI ) ) // 駆動輪の角速度[rad/s]からRoboClawの指令値[pulses/s]に変換するための定数(3ppr)
+    #define _4RES_2PI_WALL ( 4 * 3 / ( 2.0*PI ) ) // 壁越え機構の角速度[rad/s]からRoboClawの指令値[pulses/s]に変換するための定数(3ppr)
     #define GEARRATIO_WHEEL ( 1.0 ) // 駆動輪のギヤ比
     #define GEARRATIO_WALL ( 1.0 )// 壁越え機構ギヤ比
     #define MAXVEL ( 1.0 )
-    #define MAXOMEGA ( PI_ )
+    #define MAXOMEGA ( PI )
 #elif DRIVE_MODE == DRIVE_OMNI3WHEEL
     #define _2RES_PI    ( 2 * 3 / 3.141592 ) // 駆動輪の角速度[rad/s]からRoboClawの指令値[pulses/s]に変換するための定数(3ppr)
     #define WHEEL_R		( 0.019 )  
